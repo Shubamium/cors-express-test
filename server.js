e964a974896de0c73d00bb765753cc9041a53e40
Vincent Liuss <liusvinv@gmail.com>
2022-12-28 18:04:40 +0700

    Add cors header

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+// CORS Proxy || Make sure to check the hosting provider support node.js & express app 
+// Backend Code for fetching the anime list , Remove this code if you want to use static hosting
+const malUsername = "Shubamium" //Change this to your MyAnimeList username
+
+
 const express = require('express')
 const cors = require('cors')
 const app = express()
+
 
 // Enable CORS for all routes
 app.use(cors())
 
 app.get('/api', async(req, res) => {
   // Make the API request
-  const apiResponse = await fetch('https://api.myanimelist.net/v2/users/shubamium/animelist?fields=list_status&status=watching&limit=8',
+  const apiResponse = await fetch(`https://api.myanimelist.net/v2/users/${malUsername}/animelist?fields=list_status&status=watching&limit=6`,
   {
     headers: {
       'X-MAL-CLIENT-ID':"ba472ff1e5680c717f45643a0644cb34"
@@ -22,9 +28,3 @@ app.listen(3000, () => {
   console.log('Server listening on port 3000')
 })
 
-// const endpoint = `https://api.myanimelist.net/v2/users/${malUsername}/animelist?fields=list_status&status=watching&limit=8`;
-
-
-// const headerse = new Headers({
-//     'X-MAL-CLIENT-ID':"ba472ff1e5680c717f45643a0644cb34"
-// })
